3a1f0098137139b51e8f7875eb0428f77c6fcec6
Anatoly <anatoliysokolov1981@gmail.com>
2019-12-21 02:46:55 +0300

    game logic added

--- src/gamepage.js
+++ src/gamepage.js
@@ -3,7 +3,12 @@ import './assets/img';
 
 import { Table } from './js/gamepage/table';
 
-const numberOfPairCards = 9;
-const table = new Table(numberOfPairCards);
+const options = {
+  timeToRemember: 2000,
+  numberOfPairCards: 9
+};
+
+const table = new Table(options);
 
 table.init();
+
--- src/js/gamepage/table.js
+++ src/js/gamepage/table.js
@@ -3,12 +3,17 @@
 import { Card } from './card';
 
 export class Table {
-  constructor(numberOfPairCards) {
-    this.numberOfPairCards = numberOfPairCards;
+  constructor(options) {
+    this.numberOfPairCards = options.numberOfPairCards;
+    this.timeToRemember = options.timeToRemember;
     this.values = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'];
     this.suits = ['S', 'C', 'D', 'H'];
     this.hand = [];
     this.container = document.querySelector('.gamepage__cards');
+    this.guessedCard = null;
+    this.score = 0;
+    this.output = document.querySelector('.gamepage__scores');
+    this.startAgainBtn = document.querySelector('.gamepage__btn');
   }
 
   getRandomInt(max) {
@@ -64,12 +69,51 @@ export class Table {
     cards.forEach(item => item.classList.toggle('card--is-flipped'));
   }
 
+  hideAllCards() {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(item => item.classList.add('card--is-flipped'));
+  }
+
+  deletePair(id) {
+    const cardsWrappers = document.querySelectorAll('.gamepage__wrapper');
+
+    cardsWrappers.forEach(item => {
+      if (item.dataset.id === id) item.firstElementChild.remove();
+    });
+  }
+
   onCardClick(e) {
     if (e.target.tagName !== 'IMG') return;
 
-    const card = e.target.parentNode;
+    const cardsWrapper = e.target.closest('.gamepage__wrapper');
+    const card = e.target.parentElement;
+    const id = cardsWrapper.dataset.id;
+    const cards = document.querySelectorAll('.card');
 
-    card.classList.toggle('card--is-flipped');
+    if (this.guessedCard === null) {
+      this.guessedCard = id;
+      card.classList.toggle('card--is-flipped');
+    } else {
+      if (this.guessedCard === id) {
+        card.classList.toggle('card--is-flipped');
+        this.score += (cards.length / 2) * 42;
+        this.output.textContent = this.score;
+        setTimeout(() => {
+          this.deletePair(id);
+          this.guessedCard = null;
+        }, 2000);
+      } else {
+        card.classList.toggle('card--is-flipped');
+        this.guessedCard = null;
+        this.score -= (9 - cards.length / 2) * 42;
+        if (this.score < 0) this.score = 0;
+        this.output.textContent = this.score;
+        setTimeout(() => {
+          this.hideAllCards();
+        }, 2000);
+      }
+    }
   }
 
   init() {
@@ -79,7 +123,7 @@ export class Table {
     this.render();
     setTimeout(() => {
       this.flipCards();
-    }, 5000);
-    this.container.addEventListener('click', e => this.onCardClick(e));
+      this.container.addEventListener('click', e => this.onCardClick(e));
+    }, this.timeToRemember);
   }
 }
