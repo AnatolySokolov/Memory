47df63709dc9e8ccdf7c6e915cf74983192f7492
Anatoly <anatoliysokolov1981@gmail.com>
2020-01-02 17:48:21 +0300

    added open/close results funtions

--- src/endpage.js
+++ src/endpage.js
@@ -2,6 +2,28 @@ import './assets/scss/main.scss';
 import './assets/img';
 
 const score = document.querySelector('.endpage__score');
+const results = document.querySelector('.results');
+const openBtn = document.getElementById('open-results');
+const closeBtn = document.getElementById('close-results');
+
+const openResults = (container, className) => {
+  container.classList.add(className);
+};
+
+const closeResults = (container, className) => {
+  container.classList.remove(className);
+};
+
+const onOpenBtnClick = () => {
+  openResults(results, 'results--opened');
+};
+
+const onCloseBtnClick = () => {
+  closeResults(results, 'results--opened');
+};
 
 score.textContent = localStorage.getItem('score');
 localStorage.removeItem('score');
+
+openBtn.addEventListener('click', onOpenBtnClick);
+closeBtn.addEventListener('click', onCloseBtnClick);
