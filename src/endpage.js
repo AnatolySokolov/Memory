40d0c6df5a5468ee25ea895e4fbbeb637deb0989
Anatoly <anatoliysokolov1981@gmail.com>
2020-01-05 15:24:09 +0300

    refractoring: remove unnecessary plugins

--- src/endpage.js
+++ src/endpage.js
@@ -3,5 +3,9 @@
 import './assets/scss/endpage.scss';
 import './assets/img/Group 2.png';
 
-import './js/score';
 import './js/results';
+
+const score = document.querySelector('.endpage__score');
+
+score.textContent = localStorage.getItem('score') || 0;
+localStorage.removeItem('score');
--- src/js/score/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const score = document.querySelector('.endpage__score');
-
-score.textContent = localStorage.getItem('score') || 0;
-localStorage.removeItem('score');
